563c979f195f0f1b5688753a35205299dd63887d
Abhishek <abhijyaligar@gmail.com>
2026-05-10 17:37:01 +0530

    feat: initialize FastAPI application with CORS middleware and API route configuration

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://roamradar.vercel.app","https://roam-radar-theta.vercel.app","http://localhost:8000"], # Adjust in production
+    allow_origins=["https://roamradar.vercel.app","https://roam-radar-theta.vercel.app","https://roam-radar-fu5o.vercel.app/","http://localhost:8000"], # Adjust in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
